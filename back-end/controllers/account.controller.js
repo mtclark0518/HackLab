6fdd2a82176f6f34396fc34c9438eef8e67d1bc5
mtclark0518 <mtclark0518@gmail.com>
2018-03-18 17:36:58 -0600

    account and users saving to database

--- back-end/controllers/account.controller.js
+++ back-end/controllers/account.controller.js
@@ -1,25 +1,43 @@
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
 const db = require('../models/index');
-
 const Account = db.Account;
 const User = db.User;
 
 // Recieves an authenticated linkedin profile id 
-// returns user profile for linkedin account
-// registers new user and returns user profile if initial login
+// find user profile for linkedin account and return ||
+// registers new user and returns user profile
 const Login = ( req, res ) => {
-    console.log(req.body);
     Account.findOrCreate({
         where: { linkedInId : req.body.id }
     })
-    .then( (account) => {
-        if(findUser(account[0].dataValues.id)){}
-        createUser(account[0].dataValues.id, req.body)
-
-    })
-    .then(user => {
-        res.json(user);
+    .then( acc => {
+        // variable for account info
+        const account = acc[0].dataValues
+        // locate user profile for account
+        User.findOne({where:{
+            AccountId: account.id
+        }})
+        .then( user => {
+            if(user){
+                // response with account as json
+                res.json(user);
+            } else {
+                // create new user profile from the request info
+                User.create({
+                    AccountId: account.id,
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    headline: req.body.headline,
+                    industry: req.body.industry,
+                    pictureUrl: req.body.pictureUrl,
+                    summary: req.body.summary
+                })
+                .then( (user, error) => {
+                    if (error) return console.log('errror: ' + error);
+                    // respond with newly created user profile
+                    res.json(user);
+                });
+            }
+        });
     });
 };
 
@@ -27,40 +45,5 @@ const Login = ( req, res ) => {
 module.exports = {
     Login
 }
-
-// PRIVATE METHODS
-
-// check if user exists for account returns user || false
-const findUser =  account  => {
-    if(!account){
-        console.log('new account')
-    }
-    User.findOne({where:{
-        AccountId: account
-    }}).then(user => {
-        if(!user){return false}
-        else return user;
-    })
-}
-
-// returns new user with linkedin profile information
-const createUser =  (id, account)  => {
-    console.log(account);
-    User.create({
-        AccountId: id,
-        firstName: account.firstName,
-        lastName: account.lastName,
-        headline: account.headline,
-        industry: account.industry,
-        pictureUrl: account.pictureUrl,
-        summary: account.summary
-
-    })
-    .then( (user, error) => {
-        if (error) console.log('errror: ' + error);
-        console.log('user created'); console.log(user);
-        return user;
-    });
-};
 
 
