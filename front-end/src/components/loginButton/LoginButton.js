1986cc9f39344b2466cbc66e991a7e7850a7ce55
Courtney Fay <fay.courtney@gmail.com>
2018-03-18 13:54:29 -0600

    tweaking the API call to get every LI field at one time

--- front-end/src/components/loginButton/LoginButton.js
+++ front-end/src/components/loginButton/LoginButton.js
@@ -8,6 +8,8 @@ class LoginButton extends Component {
   }
 
   createProfile(data) {
+
+    console.log(data)
     requestAPI("profile/create", "POST", data)
       .then(res => {
         console.log(res);
@@ -21,7 +23,7 @@ class LoginButton extends Component {
             <script>
               function onLinkedInLoad() {
                 window.IN.Event.on(window.IN, "auth", () => {
-                    window.IN.API.Raw("/people/~")
+                    window.IN.API.Raw("/people/~:(id,first-name,last-name,headline,industry,location,summary,picture-url::(original),public-profile-url)?format=json")
                         .result(data => {
                             this.createProfile(data);
                         })
