14f6441e0b295f5cc572044a8811baf1102302b7
mtclark0518 <mtclark0518@gmail.com>
2018-03-18 14:22:01 -0600

    updates routing to use account controller

---
+++ back-end/controllers/account.controller.js
@@ -0,0 +1,15 @@
+const db = require('../models/index');
+
+const Account = db.Account;
+const User = db.User;
+
+
+
+createNew(req,res) {
+    console.log(req.body);
+}
+
+
+module.exports = {
+    createNew
+}
