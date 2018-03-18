12d60edbae6126af18cd42d7bfa0a8d37a3999ee
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-03-18 11:15:55 -0600

    messing with splash component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginButton from './components/LoginButton';
+import Splash from './components/splash/splash';
 
 class App extends Component {
   render() {
 
     return (
       <div className="App">
-        <header className="App-header">
+       
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        
         <LoginButton />
+        <Splash />
       </div>
     );
   }
