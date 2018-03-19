a94dbf2b943939ababd59f8d46ac57d474105ae2
mtclark0518 <mtclark0518@gmail.com>
2018-03-19 17:16:25 -0600

    changes account id to uuid

--- back-end/models/account.js
+++ back-end/models/account.js
@@ -1,15 +1,23 @@
 module.exports = (sequelize, Sequelize) => {
     const model = sequelize.define('Account', {
         id: {
-            type: Sequelize.INTEGER,
+            type: Sequelize.UUID,
             primaryKey: true,
-            autoIncrement: true
+            defaultValue: Sequelize.UUIDV4
         },
         linkedInId: {
             type: Sequelize.STRING,
             allowNull: false,
             unique: true
-        }
+        },
     });
+// TODO
+    // model.prototype.hash = function(password) {
+    //     return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+    // };
+    // model.prototype.validPassword = function(attempted, encrypted) {
+    //     return bcrypt.compareSync(attempted, encrypted);
+    // };
+
     return model
 }
