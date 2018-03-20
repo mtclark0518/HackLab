16dcd7b698388eb4d8ca4df12207a279f006afd9
mtclark0518 <mtclark0518@gmail.com>
2018-03-19 20:22:40 -0600

    pulls  logic out of script and breaks auth into steps

--- front-end/src/components/loginButton/LoginButton.js
+++ front-end/src/components/loginButton/LoginButton.js
@@ -2,28 +2,34 @@ import React, {Component} from 'react';
 
 class LoginButton extends Component {
 
-	// after a sucesseful linkedInLogin
+    componentDidMount(){
+        this.onLinkedInLoad();
+    }
+
+    // sets the auth event listener
+    onLinkedInLoad() {
+        window.IN.Event.on(window.IN, "auth", this.getProfileData.bind(this)); 
+    }
+
+    // after a sucesseful linkedInLogin
 	onLinkedIn(data){
-		this.props.loginOrCreate(data)
-	}
+        this.props.loginOrCreate(data)
+    }
+
+    onError(error){
+        console.log(error);
+    }
+
+    // requests profile information from linkedIn
+    getProfileData(){
+        window.IN.API.Raw("/people/~:(id,first-name,last-name,headline,industry,location,summary,picture-url::(original),public-profile-url)")
+        .result( data => {this.onLinkedIn(data); })
+        .error(this.onError);
+    }
 
     render() {
       return (
-          <script type="IN/Login">
-            <script>
-              function onLinkedInLoad() {
-                window.IN.Event.on(window.IN, "auth", () => {
-                    window.IN.API.Raw("/people/~:(id,first-name,last-name,headline,industry,location,summary,picture-url::(original),public-profile-url)")
-                        .result(data => {
-                            this.onLinkedIn(data);
-                        })
-                        .error(error => {
-                            console.log(error)
-                        })
-                })
-              }
-            </script>
-          </script>
+          <script type="IN/Login"></script>
       )
     }
 }
