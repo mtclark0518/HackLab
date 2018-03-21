48237fc651db932468d1e50eac7f438c4d4ecfb2
mtclark0518 <mtclark0518@gmail.com>
2018-03-20 22:24:03 -0600

    getProfile returns profile if user is authorized

--- back-end/controllers/api.controller.js
+++ back-end/controllers/api.controller.js
@@ -5,8 +5,19 @@ const auth = require('../middleware/auth');
 
 
 const getProfile = (req, res) => {
-    console.log('hi')
-    res.json('hi');
+    console.log(req.user)
+    User.findOne({
+        where: {
+            AccountId: req.user.id
+        }
+    })
+    .then( user => {
+        if(user){
+            console.log(user)
+            res.json(user)
+        }
+        if(!user){ res.json({message:"problem finding the user profile"})}
+    })
 }
 
 module.exports = {
